e32e7bc11fefb8a9069db05b3882fb0ae70736d0
ka1dyn <wt0630@naver.com>
2026-04-04 04:44:15 +0900

    🩹 Delete cache

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,7 +2,6 @@ import "./index.css";
 
 import React from "react";
 import { Folder, Composition } from "remotion";
-import { DesignTest3 } from "./projects/design-test3/design-test3";
 import { DesignTest2 } from "./projects/design-test2/design-test2";
 import { DesignTest1 } from "./projects/design-test1/design-test1";
 import { WebParadiam } from "./projects/web-paradiam/web-paradiam";
@@ -12,15 +11,6 @@ import { VIDEO_FPS, VIDEO_WIDTH, VIDEO_HEIGHT } from "./constants/video-config";
 export const RemotionRoot: React.FC = () => {
   return (
     <Folder name="Projects">
-      <Composition
-        id="design-test3"
-        component={DesignTest3}
-        durationInFrames={16011}
-        fps={VIDEO_FPS}
-        width={VIDEO_WIDTH}
-        height={VIDEO_HEIGHT}
-      />
-
       <Composition
         id="design-test2"
         component={DesignTest2}
